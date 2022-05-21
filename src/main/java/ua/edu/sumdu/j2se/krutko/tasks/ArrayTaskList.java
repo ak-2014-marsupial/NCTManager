523a98e9572f30062c23bbb363b72c9bb982d234
Andrew Krutoy <ak36794ca@gmail.com>
2022-05-21 17:15:15 +0300

    Added ArraysTaskList

---
+++ src/main/java/ua/edu/sumdu/j2se/krutko/tasks/ArrayTaskList.java
@@ -0,0 +1,107 @@
+package ua.edu.sumdu.j2se.krutko.tasks;
+
+
+public class ArrayTaskList {
+    private int size;
+    private int capacity;
+    private Task[] listOfTask;
+
+    /**
+     * Конструктор створює список задач із розміром capacity.
+     */
+    public ArrayTaskList() {
+        capacity = 10;
+        size = 0;
+        listOfTask = new Task[capacity];
+    }
+
+    /**
+     * Метод додає задачу до списку, та динамічно розширює масив.
+     *
+     * @param task задача, якщо має значення Null - не додається до списку
+     */
+    public void add(Task task) {
+        if (task != null) {
+            if (size == capacity) {
+                grow();
+            }
+            listOfTask[size] = task;
+            size++;
+        }
+    }
+
+    private void grow() {
+        capacity = (int) (capacity * 1.5);
+        Task[] newListOfTask = new Task[capacity];
+        for (int i = 0; i < listOfTask.length; i++) {
+            newListOfTask[i] = listOfTask[i];
+        }
+        listOfTask = newListOfTask;
+    }
+
+    /**
+     * Метод видаляє задачу зі списку.
+     *
+     * @param task задача
+     * @return true- якщо задача була у списку,
+     * false- якщо задачі не було у списку
+     */
+    public boolean remove(Task task) {
+        for (int i = 0; i < this.size; i++) {
+            if (task.equals(this.listOfTask[i])) {
+                this.fastRemove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void fastRemove(int index) {
+        int lengthToMove = this.size - index - 1;
+        if (lengthToMove > 0) {
+            System.arraycopy(this.listOfTask, index + 1,
+                    this.listOfTask, index, lengthToMove);
+        }
+        this.listOfTask[--this.size] = null;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    /**
+     * Метод повертає задачу за індексом у списку.
+     *
+     * @param index індекс задачі у списку
+     * @return повертає задачу за індексом
+     */
+    public Task getTask(int index) {
+        if (index > this.size) {
+            return null;
+        }
+        return this.listOfTask[index];
+    }
+
+    /**
+     * Метод повертає підмножину задач, що заплановані на виконання.
+     * хоча б раз після часу from і не пізніше to
+     *
+     * @param from час початку інтервалу
+     * @param to   час кінця інтервалу
+     * @return повертає підмножину задач, що заплановані на виконання
+     *          хоча б раз після часу from і не пізніше to
+     */
+    public ArrayTaskList incoming(int from, int to) {
+        ArrayTaskList incomingArrayTaskList = new ArrayTaskList();
+        for (int i = 0; i < this.size; i++) {
+            if (this.listOfTask[i].nextTimeAfter(from) != -1
+                    && this.listOfTask[i].nextTimeAfter(from) < to) {
+                incomingArrayTaskList.add(this.listOfTask[i]);
+            }
+        }
+        return incomingArrayTaskList;
+    }
+
+}
+
+
--- src/main/java/ua/edu/sumdu/j2se/krutko/tasks/Main.java
+++ src/main/java/ua/edu/sumdu/j2se/krutko/tasks/Main.java
@@ -1,31 +1,48 @@
 package ua.edu.sumdu.j2se.krutko.tasks;
 
-//import java.util.ArrayList;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<Task> list = new ArrayList<>();
 
         Task task1 = new Task("Обід із гарною дівчиною", (24 + 8 * 30) * 24 + 16);
-        Task task2 = new Task("Ранкова пробіжка", 3 * 30 * 24, 9 * 30 * 24, 24);
-        Task task3 = new Task("Приймання ліків", (20 + 8 * 30) * 24, (28 + 8 * 30) * 24, 12);
+        Task task2 = new Task("Ранкова пробіжка", 3 * 30 * 24,
+                9 * 30 * 24, 24);
+        Task task3 = new Task("Приймання ліків", (20 + 8 * 30) * 24,
+                (28 + 8 * 30) * 24,
+                12);
         Task task4 = new Task("Зустріч з друзями", (9 * 30) * 24 + 18);
         task1.setActive(true);
         task2.setActive(true);
         task3.setActive(true);
         task4.setActive(true);
-        list.add(task1);
-        list.add(task2);
-        list.add(task3);
-        list.add(task4);
-        for (Task task : list) {
+
+
+        ArrayTaskList arrayTaskList = new ArrayTaskList();
+        arrayTaskList.add(task1);
+        arrayTaskList.add(task2);
+        arrayTaskList.add(task3);
+        arrayTaskList.add(task4);
+        System.out.println("size = " + arrayTaskList.size());
+
+        for (int i = 0; i < arrayTaskList.size(); i++) {
+            Task task = arrayTaskList.getTask(i);
+            System.out.println(task.getTitle() + " " + task.getStartTime());
+
+        }
+        System.out.println("Remove task1 " + arrayTaskList.remove(task1));
+        System.out.println("size = " + arrayTaskList.size());
+        int from = (25 + 8 * 30) * 24 + 8;
+        int to = (26 + 8 * 30) * 24 + 8;
+        System.out.println();
+
+        ArrayTaskList incoming = arrayTaskList.incoming(from, to);
+        System.out.println("incoming size " + incoming.size());
+
+        for (int i = 0; i < incoming.size(); i++) {
+            Task task = incoming.getTask(i);
             System.out.println(task.getTitle());
         }
-
 
     }
 
